defcbc315943a2d3c173ddf8904688536c3e620c
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 15:34:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  
+  <form id="create-task-form" action="#" method="post">
+      <label for="new-task-description">Task description:</label>
+      <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+      <input type="submit" value="Create New Task">
+    </form>
+    const 
 });
