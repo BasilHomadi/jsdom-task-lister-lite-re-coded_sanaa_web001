ab395c49aceacf4fd70e1643a38e072a3e8ab1c4
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 15:46:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskForm = document.getElementById("create-task-form")
     const taskDescription = document.getElementById("new-task-description")
     const taskUl = document.getElementById("tasks")
-    taskForm.addEventListener('submit', createNewTask())
+    taskForm.addEventListener('submit', createNewTask
 });
 
 
