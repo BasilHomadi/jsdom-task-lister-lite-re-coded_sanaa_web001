e1878353e97026cc37b06b0b2714e1c38d7b7a3e
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 15:36:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  <form id="create-task-form" action="#" method="post">
-      <label for="new-task-description">Task description:</label>
-      <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
-      <input type="submit" value="Create New Task">
-    </form>
     const taskForm = document.getElementById("create-task-form")
     const taskDescription = document.getElementById("new-task-description")
     
